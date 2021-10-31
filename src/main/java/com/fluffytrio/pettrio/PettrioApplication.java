587290ad0ca27deca7936f4f55c231661d32a636
limetree-j <limetree.jdh@gmail.com>
2021-10-31 18:59:42 +0900

    Initial Commit

---
+++ src/main/java/com/fluffytrio/pettrio/PettrioApplication.java
@@ -0,0 +1,13 @@
+package com.fluffytrio.pettrio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PettrioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PettrioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fluffytrio/pettrio/PettrioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluffytrio.pettrio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PettrioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
